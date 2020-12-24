2a1f97e5aae652d5966148832bf4e90e54a58358
marsonshine <ms27946@outlook.com>
2020-12-24 22:58:55 +0800

    fix: 如何正确使用 go

--- book/ConcurrencyInGo/src/memoryleak/main.go
+++ book/ConcurrencyInGo/src/memoryleak/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 func main() {
-
+	memoryleak2()
+	notmemoryleak2()
 }
 
 func memoryleak() {
@@ -58,4 +60,54 @@ func notmemoryleak() {
 	}()
 	<-terminated //4
 	fmt.Println("Done.")
+}
+
+// 给正在阻塞的 channel 写入值的情况
+func memoryleak2() {
+	newRandStream := func() <-chan int {
+		randStream := make(chan int)
+		go func() {
+			defer fmt.Println("newRandStream closure exited.")
+			defer close(randStream)
+			for { // 这里一旦触发 newRannStream 就会无限循环写入数据至通道
+				randStream <- rand.Int()
+			}
+		}()
+		return randStream
+	}
+
+	randStream := newRandStream()
+	fmt.Println("3 random ints:")
+	for i := 1; i <= 3; i++ { // 这里只接收三次数据，所以 randStream 函数内的写入通道的 goroutine 还是一直会存在，所占用的内存也不会被回收
+		fmt.Printf("%d: %d\n", i, <-randStream)
+	}
+}
+
+// 这里就用另一个只接收的通道来通知取消，来防止内存泄漏
+func notmemoryleak2() {
+	newRandStream := func(done <-chan interface{}) <-chan int {
+		randStream := make(chan int)
+		go func() {
+			defer fmt.Println("newRandStream closure exited.")
+			defer close(randStream)
+			for { // 这里一旦触发 newRannStream 就会无限循环写入数据至通道
+				select {
+				case randStream <- rand.Int():
+				case <-done:
+					return
+				}
+			}
+		}()
+		return randStream
+	}
+
+	done := make(chan interface{})
+	randStream := newRandStream(done)
+	fmt.Println("3 random ints:")
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("%d: %d\n", i, <-randStream)
+	}
+	close(done) // 通知流程已经结束
+	// 模式耗时
+	time.Sleep(1 * time.Second)
 }
