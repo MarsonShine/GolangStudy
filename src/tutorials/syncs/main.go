d491053abdaf542d0d26655b8df1600a65af6334
marsonshine <ms27946@163.com>
2021-06-24 13:48:40 +0800

    fix: sync.Once 理解

---
+++ src/tutorials/syncs/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var once sync.Once
+
+func main() {
+	pool()
+
+	for i := 0; i < 10; i++ {
+		once.Do(func() {
+			fmt.Println("execed...")
+		})
+	}
+
+	duplicate(once)
+}
+
+func duplicate(once sync.Once) {
+	for i := 0; i < 10; i++ {
+		once.Do(func() {
+			fmt.Println("execed2...")
+		})
+	}
+}
+
+func pool() {
+	myPool := &sync.Pool{
+		New: func() interface{} {
+			fmt.Println("Creating new instance.")
+			return struct{}{}
+		},
+	}
+	myPool.Get()             //1
+	instance := myPool.Get() //1
+	myPool.Put(instance)     //2
+	myPool.Get()             //3
+}
