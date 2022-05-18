7f5414c3c04e05fa1785264d6e632e4781849cb0
marsonshine <ms27946@163.com>
2022-05-18 18:03:14 +0800

    feat: http,socket,httpclient

--- src/go-core-36/29-net/socket/main.go
+++ src/go-core-36/29-net/socket/main.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"runtime"
 	"syscall"
+	"time"
 )
 
 func main() {
@@ -49,4 +50,45 @@ User-Agent: Dialer/%s
 		return
 	}
 	fmt.Println()
+
+	dialArgsList := []dailArgs{
+		{
+			"tcp",
+			"google.cn:80",
+			time.Millisecond * 500,
+		},
+		{
+			"tcp",
+			"google.cn:80",
+			time.Second * 2,
+		},
+		{
+			"tcp",
+			"google.cn:80",
+			time.Minute * 4,
+		},
+	}
+	for _, args := range dialArgsList {
+		fmt.Printf("Dial %q with network %q and timeout %s ...\n",
+			args.address, args.network, args.timeout)
+		ts1 := time.Now()
+		conn, err := net.DialTimeout(args.network, args.address, args.timeout)
+		ts2 := time.Now()
+		fmt.Printf("Elapsed time: %s\n", time.Duration(ts2.Sub(ts1)))
+		if err != nil {
+			fmt.Printf("dial error: %v\n", err)
+			fmt.Println()
+			continue
+		}
+		defer conn1.Close()
+		fmt.Printf("The local address: %s\n", conn.LocalAddr())
+		fmt.Printf("The remote address: %s\n", conn.RemoteAddr())
+		fmt.Println()
+	}
+}
+
+type dailArgs struct {
+	network string
+	address string
+	timeout time.Duration
 }
